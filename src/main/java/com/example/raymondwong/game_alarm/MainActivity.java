59c2507fbd3efd3b05e02c6aa78d1e7436f7786c
TheMediocreGatsby <raymondwong6398@gmail.com>
2016-02-27 14:00:14 -0500

    Added files via upload

---
+++ src/main/java/com/example/raymondwong/game_alarm/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.raymondwong.game_alarm;
+
+import android.provider.AlarmClock;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void AlarmClock(){
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
